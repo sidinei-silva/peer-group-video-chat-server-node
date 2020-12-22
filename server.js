408694c7a43cb9dc7a3389aea4ef00fc873610f9
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-21 22:34:30 -0300

    Incluindo socket io e peer

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const io = require('socket.io')(server,{
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const { ExpressPeerServer } = require('peer');
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
+app.use('/peerjs', peerServer);
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {console.log(`Listening in port: ${PORT}`)})
